99743a08f6509cf77ab8aac85267c2a7e8008084
tumu-10 <samueltumusiimerobin2000@gmail.com>
2026-01-08 12:58:00 +0300

    changes

--- database/migrations/2026_01_06_115927_create_tenants_table.php
+++ database/migrations/2026_01_06_115927_create_tenants_table.php
@@ -8,12 +8,14 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (!Schema::hasTable('tenants')) {
         Schema::create('tenants', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name')->nullable()->unique(); // or whatever you want to identify tenants
             $table->json('data')->nullable(); // optional JSON data for tenancy
             $table->timestamps();
         });
+         }
     }
 
     public function down(): void
