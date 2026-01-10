b32d39a7f5088b4fafb0a6fa68bdf27dd0610930
kagimu <kagimujayp01@gmail.com>
2026-01-10 05:35:58 +0300

    I AM DONE-2

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             Schema::create('users', function (Blueprint $table) {
                 $table->id();
                 $table->uuid('tenant_id')->nullable();
-                $table->foreign('tenant_id')->references('id')->on('schools')->onDelete('cascade');
                 $table->string('firstName');
                 $table->string('lastName');
                 $table->string('userType')->nullable();
